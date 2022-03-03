e401c2b87f10899c8a31e477e76fb15dfe354f85
Sam <samangarc22@gmail.com>
2022-03-03 16:52:57 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run --prefix docs build
 
 # navigate into the build output directory
-cd docs/src/.vuepress/dist
+# cd docs/src/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -18,6 +18,6 @@ git commit -m 'deploy site'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:plant-dudes/plantaverse.git master:gh-pages
+git push -f git@github.com:Plant-Dudes/plantaverse.git master:gh-pages
 
-cd -
+# cd -
